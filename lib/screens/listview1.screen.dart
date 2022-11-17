2f1418fb0d32aa666c52489cd7347554c8f46fe5
Cristian Alfredo Manzo Parra <ca.manzo973@uniandes.edu.co>
2022-11-16 21:36:40 -0500

    ListTile add icons leading trailing

--- lib/screens/listview1.screen.dart
+++ lib/screens/listview1.screen.dart
@@ -3,20 +3,28 @@ import 'package:flutter/material.dart';
 class Listview1Screen extends StatelessWidget {
   const Listview1Screen({Key? key}) : super(key: key);
 
+  final options = const [
+    'Roger Feder',
+    'Nadal',
+    'Murray',
+    'Deltoro',
+    'Serena W.'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text('ListView Tipo 1'),
         ),
-        body: ListView(
-          children: const [
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo')
-          ],
-        ));
+        body: ListView(children: [
+          ...options
+              .map((option) => ListTile(
+                    leading: const Icon(Icons.sports_tennis_outlined),
+                    title: Text(option),
+                    trailing: const Icon(Icons.arrow_forward_ios),
+                  ))
+              .toList()
+        ]));
   }
 }
