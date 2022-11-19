48c75fc52617d537d2315a23119bbface6b39b1c
Cristian Alfredo Manzo Parra <ca.manzo973@uniandes.edu.co>
2022-11-19 18:55:41 -0500

    FEAT: Navegar nueva pantalla

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -1,3 +1,4 @@
+import 'package:cr_components/screens/listview1.screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,12 +7,26 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Components Flutter'),
-      ),
-      body: const Center(
-        child: Text('HomeScreen'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Components Flutter'),
+          elevation: 0,
+        ),
+        body: ListView.separated(
+          itemBuilder: (context, index) => ListTile(
+            leading: const Icon(Icons.read_more_outlined),
+            title: const Text('Route name'),
+            onTap: () {
+              // final route = MaterialPageRoute(
+              //   builder: (context) => const Listview1Screen(),
+              // );
+              // Navigator.push(context, route);
+              // // sirve para eleminiar el atras ruta antrior (no volver a login)
+              // // Navigator.pushReplacement(context, route);
+              Navigator.pushNamed(context, 'card');
+            },
+          ),
+          separatorBuilder: (_, __) => const Divider(),
+          itemCount: 10,
+        ));
   }
 }
